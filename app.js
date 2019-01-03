d4a30b295f85dbef02f8fa2f2b851f790c1b27dc
shobhit1997 <shobhit.shobhitagarwal.agarwal@gmail.com>
2019-01-03 20:09:24 +0530

    fixes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var json2xls = require('json2xls');
 // app.use(express.static(publicPath));
 app.use(function(req,res,next){
 	res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Expose-Headers', 'x-auth');
+    res.setHeader('Access-Control-Expose-Headers', 'x-auth , token , username');
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept , x-auth , username , token');
   
